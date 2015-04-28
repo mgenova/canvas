85a814c093eacb7fe7b666f8d793bb948f2f6e64
Derrick Lord <derrick.lord@gmail.com>
2015-04-28 13:32:05 -1000

    Fix broken images

--- canvas.js
+++ canvas.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
   var container = "<div id='header-img'></div>";
   var greeting = "<center><h1 id='header-title' class='lead'>Welcome Myron B. Thompson Parents & Students</h1></center>";
   var paragraph = "<center><!--<p class='header-txt'>Classes begin on Tuesday, July 29th, all courses will appear under the <strong>Courses</strong> menu item above.</p>--></center>";
-  var message_center ="<center><a href='/conversations?#filter=type=inbox'><img id='message-icon' src='https://mbta.me/web/static/canvas/message2.png' /></a>  <span class='lead' style='margin-left:5px;'>Message Center (Inbox)</span></center>";
+  var message_center ="<center><a href='/conversations?#filter=type=inbox'><img id='message-icon' src='http://derricklord.github.io/canvas/message2.png' /></a>  <span class='lead' style='margin-left:5px;'>Message Center (Inbox)</span></center>";
   var getting_started = "<center> <a class='btn btn-success' href='https://mbta.instructure.com/courses/433' style='margin:10px;'>Getting Started</a><a class='btn btn-default' target='_blank' href='http://webmail.ethompson.org' style='margin:10px;'>School Gmail</a><a class='btn btn-default' href='http://mbta.me' style='margin:10px;'>School Website</a><a class='btn btn-default' href='https://mbta.instructure.com/courses/432' style='margin:10px;'>Elementary Office</a><a class='btn btn-danger' href='http://mbta.me/support/ticket' style='margin:10px;'>Technical Help</a></center>";
   var content = $("#dashboard").prepend(container);
   var contentdiv = $("#header-img");
@@ -29,7 +29,7 @@ $(document).ready(function(){
   
   
   var container2 = "<div id='mobile-header'></div>";
-  var mobile = "<center><a href='https://itunes.apple.com/us/app/canvas-by-instructure-cloud/id480883488?mt=8' target='_blank'><img src='https://mbta.me/web/static/canvas/apple_itunes.png'/></a> <a href='https://play.google.com/store/apps/details?id=com.instructure.candroid' target='_blank'><img src='https://mbta.me/web/static/canvas/google_play.png'/></a></center>"; 
+  var mobile = "<center><a href='https://itunes.apple.com/us/app/canvas-by-instructure-cloud/id480883488?mt=8' target='_blank'><img src='http://derricklord.github.io/canvas/apple_itunes.png'/></a> <a href='https://play.google.com/store/apps/details?id=com.instructure.candroid' target='_blank'><img src='http://derricklord.github.io/canvas/google_play.png'/></a></center>"; 
   var mobile_paragraph ="<center><p class='lead'> Access course information from your mobile device</p></center>"
   var content2 = $("#dashboard").append(container2);
   var contentdiv2 = $("#mobile-header");
